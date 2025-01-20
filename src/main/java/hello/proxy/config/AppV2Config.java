c3d84eafeca235d2219dba8199018fb3d3431e72
MinJunChoi <chlalswns200@naver.com>
2025-01-20 13:43:00 +0900

    add example v2

---
+++ src/main/java/hello/proxy/config/AppV2Config.java
@@ -0,0 +1,22 @@
+package hello.proxy.config;
+
+import hello.proxy.app.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppV2Config {
+
+    @Bean
+    public OrderControllerV2 orderControllerV2() {
+        return new OrderControllerV2(orderServiceV2());
+    }
+    @Bean
+    public OrderServiceV2 orderServiceV2() {
+        return new OrderServiceV2(orderRepositoryV2());
+    }
+    @Bean
+    public OrderRepositoryV2 orderRepositoryV2() {
+        return new OrderRepositoryV2();
+    }
+}
